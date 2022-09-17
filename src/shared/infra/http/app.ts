95ac032dc053d3405d80075cbee3ad7dffb0a33d
Rodrigo Brocchi <rodrigo.brocchi@gmail.com>
2022-09-17 16:51:50 -0300

    configuração de instância de erro http

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,5 +1,6 @@
+import AppError from "@error/AppError";
 import cors from "cors";
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
 import helmet from "helmet";
 import http from "http";
 import "express-async-errors";
@@ -15,6 +16,25 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.status(200).json({ status: "ok" });
 });
+
+app.use(
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        status: "error",
+        message: err.message,
+      });
+    }
+
+    console.error(err);
+
+    return response.status(500).json({
+      status: "error",
+      message: `internal server error - ${err.message}`,
+    });
+  }
+);
 
 const serverHttp = http.createServer(app);
 
