c2879c5bed91f7807af0b598e628f23347f5b67a
Rodrigo Brocchi <rodrigo.brocchi@gmail.com>
2022-09-17 19:31:43 -0300

    Config app: remove request test

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -3,9 +3,6 @@ import cors from "cors";
 import express, { Request, Response, NextFunction } from "express";
 import helmet from "helmet";
 import http from "http";
-import qs from "qs";
-
-import typeformApi from "@shared/infra/http/service/typeform";
 
 import "express-async-errors";
 
@@ -15,36 +12,19 @@ const app = express();
 
 app.use(cors());
 
-app.use(helmet());
+app.use(helmet({
+  hidePoweredBy: true
+}));
 
 app.use(express.json());
 
 app.use(router);
 
-// async function chamada() {
-//   const params = {
-//     page_size: 200,
-//   };
-
-//   const responseApi = await typeformApi.get("/forms", {
-//     params,
-//     paramsSerializer: (params) => {
-//       return qs.stringify(params);
-//     },
-//   });
-//   const texto = JSON.stringify(responseApi.data);
-//   return texto;
-// }
-
-// const result = chamada();
-
 app.get("/", (req, res) => {
-  res.status(200).json({ status: "ok" });
-  // console.log(result);
+  res.status(200).json({ api: "ludica", status: "ok" });
 });
 
 app.use(
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   (err: Error, request: Request, response: Response, next: NextFunction) => {
     if (err instanceof AppError) {
       return response.status(err.statusCode).json({
